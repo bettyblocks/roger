5118020ae3e3cd91da088dde186415fc93c95de5
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-11-04 21:17:01 +0100

    Add state manager binary (de)serialization

--- lib/roger/application/state_manager.ex
+++ lib/roger/application/state_manager.ex
@@ -63,6 +63,40 @@ defmodule Roger.Application.StateManager do
   defmodule State do
     @moduledoc false
     defstruct application: nil, cancel_set: nil, queue_set: nil, execute_set: nil, paused: MapSet.new
+
+    @keysets ~w(cancel_set queue_set execute_set)a
+
+    def serialize(struct) do
+      {:ok, cancel_set} = KeySet.get_state(struct.cancel_set)
+      {:ok, queue_set} = KeySet.get_state(struct.queue_set)
+      {:ok, execute_set} = KeySet.get_state(struct.execute_set)
+      %{
+        cancel_set: cancel_set,
+        queue_set: queue_set,
+        execute_set: execute_set,
+        paused: struct.paused
+      }
+      |> :erlang.term_to_binary
+    end
+
+    def deserialize(data) do
+      struct = :erlang.binary_to_term(data)
+      {:ok, cancel_set} = KeySet.start_link(state: struct[:cancel_set])
+      {:ok, queue_set} = KeySet.start_link(state: struct[:queue_set])
+      {:ok, execute_set} = KeySet.start_link(state: struct[:execute_set])
+      %State{
+        cancel_set: cancel_set,
+        queue_set: queue_set,
+        execute_set: execute_set,
+        paused: struct.paused}
+    end
+
+    def merge(a, b) do
+      KeySet.union(a.cancel_set, b.cancel_set)
+      KeySet.union(a.queue_set, b.queue_set)
+      KeySet.union(a.execute_set, b.execute_set)
+      %__MODULE__{a | paused: MapSet.union(a.paused, b.paused)}
+    end
   end
 
   def init([application]) do
@@ -130,4 +164,5 @@ defmodule Roger.Application.StateManager do
   def handle_call(:queue_get_paused, _from, state) do
     {:reply, state.paused, state}
   end
+
 end
