dbb0efc8784c611a2c60b29cff6987a23224cdee
nulian <peter.arentsen@quicknet.nl>
2018-08-23 16:01:08 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Roger.Mixfile do
 
   def project do
     [app: :roger,
-     version: "2.0.1",
+     version: "2.1.0",
      elixir: ">= 1.5.1",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
