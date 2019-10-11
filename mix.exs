dfd2006cdff3bda076106b2dea96b283ed0fed20
Peter Arentsen <peter.arentsen@holder.nl>
2019-10-11 13:58:04 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Roger.Mixfile do
 
   def project do
     [app: :roger,
-     version: "2.2.0",
+     version: "2.2.1",
      elixir: ">= 1.5.1",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
