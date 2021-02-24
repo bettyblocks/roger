14195b5dee1df9cdd6d3561039c5e537c88c27f8
Peter <peter.arentsen@bettyblocks.com>
2021-02-24 10:23:13 +0100

    chore: correctly format mix

--- mix.exs
+++ mix.exs
@@ -2,19 +2,20 @@ defmodule Roger.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :roger,
-     version: "2.4.2",
-     elixir: ">= 1.6.0",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: description(),
-     package: package(),
-     source_url: "https://github.com/bettyblocks/roger",
-     homepage_url: "https://github.com/bettyblocks/roger",
-     deps: deps(),
-     docs: [extras: ["docs/overview.md", "docs/configuration.md"]]
-  ]
+    [
+      app: :roger,
+      version: "2.4.2",
+      elixir: ">= 1.6.0",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/bettyblocks/roger",
+      homepage_url: "https://github.com/bettyblocks/roger",
+      deps: deps(),
+      docs: [extras: ["docs/overview.md", "docs/configuration.md"]]
+    ]
   end
 
   defp elixirc_paths(:prod), do: ["lib"]
